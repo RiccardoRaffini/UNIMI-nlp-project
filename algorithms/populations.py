c4a8f38f9d6738bca2ab2d3e2c5527fb0fa09027
RiccardoRaffini <riccardo.raffini@gmail.com>
2025-02-06 17:00:03 +0100

    random recipe population

--- algorithms/populations.py
+++ algorithms/populations.py
@@ -1,6 +1,9 @@
+import pandas as pd
+import random
 from abc import ABC, abstractmethod
 from copy import deepcopy
-from typing import Generic, TypeVar, Callable, List, Any, Dict
+from typing import Generic, TypeVar, Callable, List, Any, Dict, Tuple
+from tqdm import tqdm
 
 from commons.recipes import Recipe, RecipeGraph, RecipeMatrices
 
@@ -111,3 +114,20 @@ class RecipePopulation(Population[RecipeIndividual]):
         population_copy = type(self)(individuals_copies)
 
         return population_copy
+
+def random_recipes_population(recipe_universe:pd.DataFrame, individuals_number:int) -> Tuple[RecipePopulation, List[int]]:
+    recipes_population = RecipePopulation()
+    recipes_indices = []
+
+    for _ in tqdm(range(individuals_number)):
+        recipe_index = random.randint(0, len(recipe_universe)-1)
+        recipes_indices.append(recipe_index)
+
+        recipe = recipe_universe.iloc[recipe_index]
+        recipe = Recipe.from_dataframe_row(recipe)
+        recipe_graph = RecipeGraph.from_recipe(recipe)
+        recipe_graph.simplify_graph()
+        recipe_individual = RecipeIndividual.from_recipe_graph(recipe_graph)
+        recipes_population.add_individual(recipe_individual)
+
+    return recipes_population, recipes_indices
